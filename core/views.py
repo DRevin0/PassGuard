022c9573602578e5c8af27a5cd18ba638271edb0
DRevin0 <dimareva2000@gmail.com>
2025-12-19 18:07:33 +0300

    Добавил страницу проверки пароля, убрал белую обводку кнопок базового слоя,

--- core/views.py
+++ core/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def check_leak(request):
+    #Страница проверки утечки паролей
+    result = None
+    if request.method == 'POST':
+        password = request.POST.get('password', '')
+        #ЛОГИКА 
+        is_leaked = len(password)<6
+        leak_count = 0 if not is_leaked else 1
+
+        result = {
+            'password':password,
+            'is_leaked':is_leaked,
+            'leak_count':leak_count,
+            'message': 'Пароль найден в учетчке' if is_leaked else 'Пароль безопасен',
+
+        }
+    return render(request, 'core/check_leak.html', {'result':result})
