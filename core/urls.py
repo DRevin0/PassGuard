1c588f9af93e0ba4ecee503b5bf0120868a5bfd0
KurteevDA <dimaalexkurt99@gmail.com>
2025-12-22 18:22:00 +0300

    Для генератора паролей  urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('check-leak/', views.check_leak, name = 'check_leak'),
     path('check-strength/', views.check_strength, name='check_strength'),
+    path('generate-password/', views.generate_password_view, name='generate_password'),
 ]
